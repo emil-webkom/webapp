1be2a7eb406e7db7978c1cf1e054296d0b5f2ba2
Mauritz Skogøy <mauritzs@stud.ntnu.no>
2024-06-21 23:45:47 +0200

    Feat: navigation bar has been made a well as added structure to the projects. We are now coding ladies and gentlemen.

---
+++ frontend/src/components/footer/footer.tsx
@@ -0,0 +1,14 @@
+'use-client'
+
+import Link from "next/link";
+import { FC } from "react";
+
+const Footer: FC = () => {
+    return(
+        <nav>
+            {/* FILLUP */}
+        </nav>
+    )
+} 
+
+export default Footer;
