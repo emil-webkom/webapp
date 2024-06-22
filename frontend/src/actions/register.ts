e79b6fd1e16085c006c8ebbd49bd7d74b52a61ea
Nicolai Faye <nicohfaye@gmail.com>
2024-06-22 09:59:07 +0200

    Started implementing AuthJS

--- frontend/src/actions/register.ts
+++ frontend/src/actions/register.ts
@@ -1,7 +1,9 @@
 "use server";
-
+import bcrypt from "bcrypt";
 import * as z from "zod";
+import { db } from "@/lib/db";
 import { RegisterSchema } from "@/schemas";
+import { getUserByEmail } from "@/data/user";
 
 export const register = async (values: z.infer<typeof RegisterSchema>) => {
   const validatedFields = RegisterSchema.safeParse(values);
@@ -9,6 +11,23 @@ export const register = async (values: z.infer<typeof RegisterSchema>) => {
   if (!validatedFields.success) {
     return { error: "Invalid credentials!" };
   }
+
+  const { email, password, name } = validatedFields.data;
+  const hashedPassword = await bcrypt.hash(password, 10);
+
+  const existingUser = await getUserByEmail(email);
+
+  if (existingUser) {
+    return { error: "Email already in use" };
+  }
+
+  await db.user.create({
+    data: {
+      name,
+      email,
+      password: hashedPassword,
+    },
+  });
 
   return { success: "Account created" };
 };
---
+++ frontend/src/data/user.ts
@@ -0,0 +1,23 @@
+import { db } from "@/lib/db";
+
+// Some helper functions to retrieve users by email or id
+
+export const getUserByEmail = async (email: string) => {
+  try {
+    const user = await db.user.findUnique({ where: { email } });
+
+    return user;
+  } catch (error) {
+    return null;
+  }
+};
+
+export const getUserById = async (id: string) => {
+  try {
+    const user = await db.user.findUnique({ where: { id } });
+
+    return user;
+  } catch (error) {
+    return null;
+  }
+};
