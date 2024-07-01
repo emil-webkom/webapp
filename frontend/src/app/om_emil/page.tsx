69f4de9f1def11503f843ddfad1deeab8ed6cbc0
Nicolai Faye <nicohfaye@gmail.com>
2024-07-01 09:23:40 +0200

    test

---
+++ frontend/src/app/om_emil/page.tsx
@@ -0,0 +1,14 @@
+import Hero from "@/components/hero/hero1"
+
+const omEmilPage = () => {
+
+  return(
+    <div className="flex flex-col m-4">
+      <div className="flex m-14 items-center justify-center space-x-5 ">
+      <Hero title="Om Emil" undertitle="Her finner du all informasjon om Emil"/>
+    </div>
+    </div>
+  )
+};
+
+export default omEmilPage;
