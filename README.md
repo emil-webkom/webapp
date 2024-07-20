f5bbab34949840d0a5216242a498eff69236440f
Mauritz Skogøy <mauritzs@stud.ntnu.no>
2024-07-20 14:30:50 +0200

    Feat: added more backendfunctionality for handling deletion of komite and defined layout for RESTapi in root readme

--- README.md
+++ README.md
@@ -1,3 +1,11 @@
 # Webapp
 
 Nettsiden for energi og miljø-studentenes linjeforening
+
+
+
+For restAPI manipulasjon bruker vi følgende måte å transportere informasjon avhengig av http metode:
+- POST: request.body
+- Get: Ingen informasjon sendes
+- Delete: URL parameter
+- Patch: request.body
--- frontend/src/app/(protected)/upload/page.tsx
+++ frontend/src/app/(protected)/upload/page.tsx
@@ -4,11 +4,13 @@ import UploadImageForm from "@/components/action/uploadImage"
 import { useState, useEffect } from "react"
 import { KomiteLogo } from "@/types/interfaces";
 import { Hovedstyret } from "@/types/interfaces";
+import { testDeleteKomite } from "@/tests/api/delete/deleteKomite";
 
 const Upload = () => {
 
     const [styret, setStyret] = useState<Hovedstyret[]>([]);
     const [logos, setLogos] = useState<KomiteLogo[]>([]);
+    const [editKomite, setEditKomite] = useState(false);
 
     const fetchAndSetData= async () =>{
       const [styretData, logosData] = await Promise.all([
@@ -31,16 +33,72 @@ const Upload = () => {
       initData();
     }, []);
 
+    const showform = () =>{
+      setEditKomite(true);
+    }
+
+    const hidefrom = () =>{
+      setEditKomite(false);
+    }
+
+    async function slettKomite(komite:string){
+      try{
+          const endpoint = `api/komite/komite?komite=${encodeURIComponent(komite)}`;
+          const response = await fetch(endpoint,{
+              method: "DELETE",
+              // Can add headers for e.g. verification
+          });
+          if (!response.ok){
+              throw new Error(`Failed to delete komite with name: ${komite}: ${response.statusText}`);
+          }
+          console.log("Komite successfully deleted")
+          const {styretData, logosData} = await fetchAndSetData();
+          setLogos(logosData);
+      }catch(error){
+          throw error;
+      }
+    }
+
+    //RestAPI tests
+    // const TestDeleteKomite = () => {
+    //   testDeleteKomite("Kvinnekom");
+    // }
+
     return(
         <div className="flex justify-center items-center m-10">
-            <div>
-                <p>Upload Komite</p>
-                <UploadKomiteForm/>
-                <ul>
+            <div className="flex flex-col items-center">
+                <p>Komiter</p>
+                {editKomite ? (<div>
+                  <ul>
                   {logos.map((item, index) => (
                     <li key={index}>{item.komite}</li>
                   ))}
                 </ul>
+                <UploadKomiteForm/>
+                <button onClick={() => hidefrom()}
+                  className="bg-blue-500 text-white py-2 px-4 rounded">
+                  Done?
+                </button>
+                </div>):(
+                  <div className="flex flex-col items-center">
+                  <ul>
+                  {logos.map((item, index) => (
+                    <li key={index}><div className="flex justify-between space-x-10">
+                      <div>
+                        {item.komite}
+                      </div>
+                      <button onClick={() => {slettKomite(item.komite)}} className="hover:underline">Slett</button>
+                      </div></li>
+                  ))}
+                </ul>
+                <button onClick={() => showform()}
+                  className="bg-blue-500 text-white py-2 px-4 rounded">
+                  Add Komite?
+                </button>
+                </div>
+                )}
+              {/* <button onClick={()=> TestDeleteKomite()}
+                className="bg-blue-500 text-white py-2 px-4 rounded">Test Delete</button> */}
             </div>
             <div>
                 <p>Upload Styretmember</p>
