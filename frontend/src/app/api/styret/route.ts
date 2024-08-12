69db75e590557d2240af93c3647eabf483f0cab7
Nicolai Faye <nicohfaye@gmail.com>
2024-08-12 12:38:49 +0200

    bugfix: small changes

--- frontend/src/app/api/styret/route.ts
+++ frontend/src/app/api/styret/route.ts
@@ -1,5 +1,5 @@
 import { NextRequest, NextResponse } from "next/server";
-import fetchStyret from "@/utils/styret/fetchers"
+import fetchStyret from "@/utils/styret/fetchers";
 import { db } from "@/lib/db";
 
 export async function GET(request: NextRequest) {
@@ -19,10 +19,9 @@ export async function GET(request: NextRequest) {
   }
 }
 
-
 // Function for updating existing entries in table hovedstyret in DB.
 export async function PATCH(request: NextRequest) {
-    /* Takes JSON objects in the form:
+  /* Takes JSON objects in the form:
     {
         rolle : "existingRolle",
         text : "newtext",
@@ -33,75 +32,92 @@ export async function PATCH(request: NextRequest) {
     and rolle and userEmail through URL: /api/styret/[rolle]&[user]
     */
 
-    const url = new URL(request.url);
-    const rolle = url.searchParams.get("rolle");
-    const userEmail = url.searchParams.get("userID")!;
-    const body = await request.json();
-    const user = await db.user.findUnique({
-        where: {email : userEmail},
+  const url = new URL(request.url);
+  const rolle = url.searchParams.get("rolle");
+  const userEmail = url.searchParams.get("userID")!;
+  const body = await request.json();
+  const user = await db.user.findUnique({
+    where: { email: userEmail },
+  });
+
+  if (!user) {
+    return NextResponse.json({ error: "Could not find User" }, { status: 400 });
+  }
+
+  if (!rolle || !user.id) {
+    return NextResponse.json(
+      { error: "Rolle and userID are required" },
+      { status: 400 },
+    );
+  }
+  try {
+    // Check if the 'rolle' and 'userID' combination exists in the database
+    const existingRolle = await db.hovedstyret.findUnique({
+      where: {
+        id: {
+          rolle: rolle,
+          userID: user.id,
+        },
+      },
     });
 
-    if (!user){
-        return NextResponse.json({error: "Could not find User"}, {status: 400})
+    if (!existingRolle) {
+      return NextResponse.json(
+        { error: "Rolle and userID combination not found" },
+        { status: 404 },
+      );
     }
+    const updatedRolle = await db.hovedstyret.update({
+      where: {
+        id: {
+          rolle: rolle,
+          userID: user.id,
+        },
+      },
+      data: {
+        rolle: body.rolle,
+        text: body.text,
+        image: body.image,
+        isActive: body.active,
+        updatedAt: new Date(),
+      },
+    });
 
-    if (!rolle || !user.id) {
-        return NextResponse.json({ error: "Rolle and userID are required" }, { status: 400 });
-    }
-    try {
-        // Check if the 'rolle' and 'userID' combination exists in the database
-        const existingRolle = await db.hovedstyret.findUnique({
-            where: {
-                id: {
-                    rolle: rolle,
-                    userID: user.id,
-                },
-            },
-        });
-
-        if (!existingRolle) {
-            return NextResponse.json({ error: "Rolle and userID combination not found" }, { status: 404 });
-        }
-        const updatedRolle = await db.hovedstyret.update({
-            where: {
-                id: {
-                    rolle: rolle,
-                    userID: user.id,
-                },
-            },
-            data: {
-                rolle: body.rolle,
-                text: body.text,
-                image: body.image,
-                active: body.active,
-                updatedAt: new Date(),
-            },
-        });
-
-        return NextResponse.json({ message: "Rolle successfully updated", rolle: updatedRolle }, { status: 200 });
-    } catch (error) {
-        console.error("Error updating rolle in database:", error);
-        return NextResponse.json({ message: "Internal server error. Could not update rolle." }, { status: 500 });
-    }
+    return NextResponse.json(
+      { message: "Rolle successfully updated", rolle: updatedRolle },
+      { status: 200 },
+    );
+  } catch (error) {
+    console.error("Error updating rolle in database:", error);
+    return NextResponse.json(
+      { message: "Internal server error. Could not update rolle." },
+      { status: 500 },
+    );
+  }
 }
 
-export async function DELETE (request: NextRequest){
-    const url = new URL(request.url);
-    const rolle = url.searchParams.get('rolle');
-    if (!rolle){
-        return NextResponse.json({error: "Rolle name is required"},{status:400});
-    }
-    try{
-        await db.hovedstyret.delete({
-            where: {rolle: rolle},
-        });
-        return NextResponse.json({message: "Rolle successfully deleted"}, {status: 200});
-    }
-    catch(error){
-        console.error("Error deleting rolle from database:", error);
-        return NextResponse.json(
-            {message: "Internal server error"},
-            {status : 500}
-        );
-    }
+export async function DELETE(request: NextRequest) {
+  const url = new URL(request.url);
+  const rolle = url.searchParams.get("rolle");
+  if (!rolle) {
+    return NextResponse.json(
+      { error: "Rolle name is required" },
+      { status: 400 },
+    );
+  }
+  try {
+    await db.hovedstyret.delete({
+      where: { rolle: rolle },
+    });
+    return NextResponse.json(
+      { message: "Rolle successfully deleted" },
+      { status: 200 },
+    );
+  } catch (error) {
+    console.error("Error deleting rolle from database:", error);
+    return NextResponse.json(
+      { message: "Internal server error" },
+      { status: 500 },
+    );
+  }
 }
