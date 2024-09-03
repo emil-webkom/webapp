333cf62f229d49f4c37fa42ac6640695b37e0890
Mauritz Skogøy <mauritzs@stud.ntnu.no>
2024-09-03 17:01:26 +0200

    Feat: wrote some endpoints for AeresEmiler handeling

--- frontend/src/app/api/aeresemiler/route.ts
+++ frontend/src/app/api/aeresemiler/route.ts
@@ -1,6 +1,10 @@
 import { NextRequest, NextResponse } from "next/server";
 import { db } from "@/lib/db";
 
+/**
+ * Function for fetching all objects in AeresEmiler
+ * @returns An array of all objects in the table AeresEmiler
+ */
 export async function GET(request: NextRequest) {
   try {
     const aeresemilere = await db.aeresEmiler.findMany();
@@ -9,33 +13,50 @@ export async function GET(request: NextRequest) {
     console.error("Error fetching aeresEmilere:", error);
     return NextResponse.json(
       { error: "Failed to fetch aeresEmilere" },
-      { status: 500 }
+      { status: 500 },
     );
   }
 }
 
-// Takes a json body-object on the format {id: String}
-export async function DELETE(request: NextRequest) {
-  const body = await request.json(); 
-  const deleteId = body.id; 
-
-  if (!deleteId) {
-      return NextResponse.json({ error: "ID is required" }, { status: 400 });
-  }
-
+/**
+ * Creates a new instance of AeresEmiler
+ * @param data: { aar: number; type: string; navn: string }
+ * @returns An instance of the object created or generic error message.
+ */
+export async function POST(request: NextRequest) {
   try {
-      const aeresemilere = await db.aeresEmiler.delete({
-          where: { id: deleteId }
-      });
+    const body = await request.json();
+
+    const { aar, type, navn } = body;
+
+    // Validate the required fields
+    if (!aar || !type || !navn) {
       return NextResponse.json(
-        {
-          message: "Deleted aeresEmilere",
-          aeresemilere: [aeresemilere]
-        },
-        { status: 200 },
+        { error: "All fields (aar, type, navn) are required" },
+        { status: 400 },
       );
-    } catch (error) {
-      console.log("Error deleting aeresEmilere", error);
-      return NextResponse.json({ error: error }, { status: 500 });
     }
+
+    const aeresemilere = await db.aeresEmiler.create({
+      data: {
+        aar,
+        type,
+        navn,
+      },
+    });
+
+    return NextResponse.json(
+      {
+        message: "AeresEmiler created successfully",
+        aeresemilere: aeresemilere,
+      },
+      { status: 201 },
+    );
+  } catch (error) {
+    console.error("Error creating AeresEmiler", error);
+    return NextResponse.json(
+      { error: "Internal Server Error" },
+      { status: 500 },
+    );
+  }
 }
