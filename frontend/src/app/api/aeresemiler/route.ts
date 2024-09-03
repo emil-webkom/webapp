fd546b5007876db151debf2a9a6260c74d686414
Mauritz Skogøy <mauritzs@stud.ntnu.no>
2024-09-03 11:46:37 +0200

    Feat: added aeresemilerSeksjon

--- frontend/src/app/api/aeresemiler/route.ts
+++ frontend/src/app/api/aeresemiler/route.ts
@@ -2,23 +2,17 @@ import { NextRequest, NextResponse } from "next/server";
 import { db } from "@/lib/db";
 
 export async function GET(request: NextRequest) {
-    try {
-        const aeresemilere = await db.aeresEmiler.findMany();
-        return NextResponse.json(
-          {
-            message: "Retrieved aeresEmilere",
-            aeresemilere},
-          { status: 200 },
-        );
-      } catch (error) {
-        console.error("Error fetching aeresEmilere:", error); 
-        return NextResponse.json(
-          { error: "Failed to fetch aeresEmilere" }, 
-          { status: 500 }
-        );
-    }
-    
-} 
+  try {
+    const aeresemilere = await db.aeresEmiler.findMany();
+    return NextResponse.json(aeresemilere, { status: 200 });
+  } catch (error) {
+    console.error("Error fetching aeresEmilere:", error);
+    return NextResponse.json(
+      { error: "Failed to fetch aeresEmilere" },
+      { status: 500 }
+    );
+  }
+}
 
 // Takes a json body-object on the format {id: String}
 export async function DELETE(request: NextRequest) {
@@ -36,7 +30,7 @@ export async function DELETE(request: NextRequest) {
       return NextResponse.json(
         {
           message: "Deleted aeresEmilere",
-          aeresemilere
+          aeresemilere: [aeresemilere]
         },
         { status: 200 },
       );
