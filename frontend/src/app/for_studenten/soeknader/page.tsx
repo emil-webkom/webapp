519ecca6def3b3367c6b17ed27bce1c5db644c6b
MariaWembstad <mariawembstad@hotmail.com>
2024-09-04 19:17:42 +0200

    made some small changes on soeknader

--- frontend/src/app/for_studenten/soeknader/page.tsx
+++ frontend/src/app/for_studenten/soeknader/page.tsx
@@ -10,6 +10,7 @@ import { FileQuestion } from "lucide-react";
 import { Flower } from "lucide-react";
 import { Notebook } from "lucide-react";
 import { Button } from "@/components/ui/button";
+import { Mail } from "lucide-react";
 
 const soeknaderPage = () => {
   const unnagjortCards: nyStudentCardProps[] = [
@@ -70,7 +71,7 @@ const soeknaderPage = () => {
       ),
       frist: "",
       buttonText: "Se eldre søknader",
-      href: "https://www.youtube.com",
+      href: "https://drive.google.com/drive/folders/1nM-PpRLzSYRzXTxGqjNdU8iynfK6UO3p",
       icon: <FileQuestion className="h-5" />,
     },
   ];
@@ -137,6 +138,21 @@ const soeknaderPage = () => {
               Søknaden skal være sendt inn minst 4 uker før midlene skal være
               utbetalt! 
             </p>
+            <br></br>
+            <br></br>
+            <br></br>
+            <br></br>
+           
+            <p className= "font-thin text-sm">
+              Søknader sendes til styret:
+            </p>
+            <div className="flex justify-items gap-2 pt-2">
+              <Mail className="h-4 w-4 text-[#9DDBAD]"></Mail>
+            <p className ="font-thin text-xs">
+            styret@emilweb.no
+            </p>
+            </div>
+            
           </div>
 
           <div className="w-[90%] bg-[#00333A] lg:w-fit flex flex-col justify-between gap-y-4 px-12 py-5">
