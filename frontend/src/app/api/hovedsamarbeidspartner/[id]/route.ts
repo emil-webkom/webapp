311c13e42df4bacf2256eaf626671a9bb6bccf55
Mauritz Skogøy <mauritzs@stud.ntnu.no>
2024-09-21 13:29:35 +0200

    Feat: made admin page hovedsamarbeidspartner fully controllable

--- frontend/src/app/api/hovedsamarbeidspartner/[id]/route.ts
+++ frontend/src/app/api/hovedsamarbeidspartner/[id]/route.ts
@@ -1,4 +1,5 @@
 import { db } from "@/lib/db";
+import { HovedsamarbeidspartnerSchema } from "@/schemas/hovedsamarbeidspartner";
 import { NextRequest, NextResponse } from "next/server";
 
 export const revalidate = 0;
@@ -12,11 +13,11 @@ export async function DELETE(req: NextRequest,
     { params }: { params: { id: string } },
   ) {
     try {
-      await db.samarbeidspartner.delete({
+      await db.hovedsamarbeidspartner.delete({
         where: { id: params.id },
       });
       return NextResponse.json(
-        { message: "Successfully deleted samarbeidspartner" },
+        { message: "Successfully deleted HSP" },
         { status: 200 },
       );
     } catch (error) {
@@ -27,27 +28,29 @@ export async function DELETE(req: NextRequest,
     }
   }
   
-//  /**
 //  * Updates a given instance in samarbeidspartner table
 //  * @param id: string
 //  * @param data: {FILL IN LATER}
 //  * @returns Response indicating if the update was successfull.
-//  */
-//   export async function PATCH(req: NextRequest,
-//     { params }: { params: { id: string } },
-//   ) {
-//     try {
-//       await db.samarbeidspartner.delete({
-//         where: { id: params.id },
-//       });
-//       return NextResponse.json(
-//         { message: "Successfully updated samarbeidspartner" },
-//         { status: 200 },
-//       );
-//     } catch (error) {
-//       return NextResponse.json(
-//         { message: "Internal server error" },
-//         { status: 500 },
-//       );
-//     }
-//   }
+
+  export async function PATCH(req: NextRequest,
+    { params }: { params: { id: string } },
+  ) {
+    const updateData = await req.json();
+    const parsedData = HovedsamarbeidspartnerSchema.parse(updateData);
+    try {
+      await db.hovedsamarbeidspartner.update({
+        where: { id: params.id },
+        data: parsedData
+      });
+      return NextResponse.json(
+        { message: "Successfully updated hovedsamarbeidspartner" },
+        { status: 200 },
+      );
+    } catch (error) {
+      return NextResponse.json(
+        { message: "Internal server error" },
+        { status: 500 },
+      );
+    }
+  }
