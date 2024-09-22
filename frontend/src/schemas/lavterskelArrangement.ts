584b6aebb92b5e40ea5b1c0879989647ab2814aa
Mauritz Skogøy <mauritzs@stud.ntnu.no>
2024-09-22 11:52:22 +0200

    Feat: made lavterskelarrangement on admin page.

--- frontend/src/schemas/lavterskelArrangement.ts
+++ frontend/src/schemas/lavterskelArrangement.ts
@@ -4,7 +4,7 @@ export const lavTerskelArrangementSchema = z.object({
   id: z.string().cuid().optional(),
   navn: z.string().min(1, "Navn is required"),
   sted: z.string().optional(),
-  dato: z.date(),
+  dato: z.union([z.date(),z.string()]),
   type: z.string().min(1, "Type is required"),
   beskrivelse: z.string().optional(),
   userId: z.string().min(1, "User ID is required"),
